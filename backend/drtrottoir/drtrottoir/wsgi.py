ffa4231fdd7fd0623d187326c22a455810c47385
Chewing_Bever <roosensjef@gmail.com>
2023-02-21 09:44:24 +0100

    Add full linting to backend

--- backend/drtrottoir/drtrottoir/wsgi.py
+++ backend/drtrottoir/drtrottoir/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drtrottoir.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drtrottoir.settings")
 
 application = get_wsgi_application()
