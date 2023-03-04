83d28a029e2cb740393a03bd7507952e2f2c0859
bavverst <bavo.verstraeten@ugent.be>
2023-03-04 17:11:15 +0100

    Now has correct (hopefully) unit tests

--- backend/drtrottoir/tests/test_garbage_collection_schedule_views.py
+++ backend/drtrottoir/tests/test_garbage_collection_schedule_views.py
@@ -41,19 +41,6 @@ def test_garbage_collection_schedule_post():
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_template_entry_get_list():
-    entry_1 = insert_dummy_garbage_collection_schedule()
-    entry_2 = insert_dummy_garbage_collection_schedule()
-
-    client = APIClient()
-    response = client.get("/garbage_collection_schedules/")
-
-    response_ids = [e["id"] for e in response.data]
-
-    assert sorted(response_ids) == sorted([entry_1.id, entry_2.id])
-
-
-@pytest.mark.django_db
 def test_garbage_collection_schedule_template_entry_get_detail():
     entry = insert_dummy_garbage_collection_schedule()
 
@@ -66,3 +53,40 @@ def test_garbage_collection_schedule_template_entry_get_detail():
         and response.data["building"]
         == entry.building.id
     )
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedule_patch():
+    entry = insert_dummy_garbage_collection_schedule()
+    old_day = entry.for_day
+    new_day = "2002-01-09" if old_day != "2002-01-09" else "2002-03-17"
+
+    user = User.objects.create_user(username="test@gmail.com", password="test")
+
+    client = APIClient()
+    client.force_login(user)
+
+    data = {"for_day": new_day}
+
+    client.patch(f"/garbage_collection_schedules/{entry.id}/", data)
+
+    response = client.get(f"/garbage_collection_schedules/{entry.id}/")
+    assert response.data["for_day"] == new_day
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedule_delete():
+    entry = insert_dummy_garbage_collection_schedule()
+
+    user = User.objects.create_user(username="test@gmail.com", password="test")
+
+    client = APIClient()
+    client.force_login(user)
+
+    response = client.delete(
+        f"/garbage_collection_schedules/{entry.id}/"
+    )
+    assert response.status_code == 204
+
+    response = client.get(f"/garbage_collection_schedules/{entry.id}/")
+    assert response.status_code == 404
