11760507fe46363c7a8106419e1e21bfcffdf8f9
Chewing_Bever <roosensjef@gmail.com>
2023-03-04 14:19:38 +0100

    Remove unused APIView implementations

--- backend/drtrottoir/views/garbage_collection_schedule_template.py
+++ backend/drtrottoir/views/garbage_collection_schedule_template.py
@@ -1,6 +1,3 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from drtrottoir.models import (
@@ -25,53 +22,3 @@ class GarbageCollectionScheduleTemplateViewSet(ModelViewSet):
 
     queryset = GarbageCollectionScheduleTemplate.objects.all()
     serializer_class = GarbageCollectionScheduleTemplateSerializer
-
-
-class GarbageCollectionScheduleTemplateEntryApiView(APIView):
-    permission_classes = []
-
-    def get(self, request, *args, **kwargs):
-        """ """
-        serializer = GarbageCollectionScheduleTemplateEntrySerializer(
-            GarbageCollectionScheduleTemplateEntry.objects, many=True
-        )
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        """ """
-        if request.user is None:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        # TODO - check user permissions
-
-        serializer = GarbageCollectionScheduleTemplateEntrySerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class GarbageCollectionScheduleTemplateApiView(APIView):
-    permission_classes = []
-
-    def get(self, request, *args, **kwargs):
-        """ """
-        serializer = GarbageCollectionScheduleTemplateSerializer(
-            GarbageCollectionScheduleTemplate.objects, many=True
-        )
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        """ """
-        if request.user is None:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        # TODO - check user permissions
-
-        serializer = GarbageCollectionScheduleTemplateSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
