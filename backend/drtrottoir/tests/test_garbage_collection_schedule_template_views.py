c5d00afe7e4b904a2d7bddb3ef56db88a766160e
Chewing_Bever <roosensjef@gmail.com>
2023-03-04 16:15:10 +0100

    Add PATCH & DELETE tests for garbage collection schedule templates

--- backend/drtrottoir/tests/test_garbage_collection_schedule_template_views.py
+++ backend/drtrottoir/tests/test_garbage_collection_schedule_template_views.py
@@ -8,30 +8,7 @@ from drtrottoir.models import User
 from .dummy_data import (
     insert_dummy_building,
     insert_dummy_garbage_collection_schedule_template,
-    insert_dummy_garbage_collection_schedule_template_entry,
-    insert_dummy_garbage_type,
 )
-
-
-@pytest.mark.django_db
-def test_garbage_collection_schedule_template_post():
-    building = insert_dummy_building()
-
-    data = {"name": "dummy schedule", "building": building.id}
-
-    user = User.objects.create_user(username="test@gmail.com", password="test")
-
-    client = APIClient()
-    client.force_login(user)
-
-    response = client.post(
-        "/garbage_collection_schedule_templates/",
-        json.dumps(data),
-        content_type="application/json",
-    )
-
-    assert response.data == {"id": 1, "name": "dummy schedule", "building": building.id}
-    assert response.status_code == 201
 
 
 @pytest.mark.django_db
@@ -61,58 +38,54 @@ def test_garbage_collection_schedule_template_get_detail():
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_template_entry_post():
-    template = insert_dummy_garbage_collection_schedule_template()
-    garbage_type = insert_dummy_garbage_type()
+def test_garbage_collection_schedule_template_post():
+    building = insert_dummy_building()
 
-    data = {
-        "day": 4,
-        "garbage_type": garbage_type.id,
-        "garbage_collection_schedule_template": template.id,
-    }
+    data = {"name": "dummy schedule", "building": building.id}
 
     user = User.objects.create_user(username="test@gmail.com", password="test")
 
     client = APIClient()
     client.force_login(user)
+
     response = client.post(
-        "/garbage_collection_schedule_template_entries/",
+        "/garbage_collection_schedule_templates/",
         json.dumps(data),
         content_type="application/json",
     )
 
-    assert response.data == {
-        "id": 1,
-        "day": 4,
-        "garbage_type": garbage_type.id,
-        "garbage_collection_schedule_template": template.id,
-    }
+    assert response.data == {"id": 1, "name": "dummy schedule", "building": building.id}
     assert response.status_code == 201
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_template_entry_get_list():
-    entry_1 = insert_dummy_garbage_collection_schedule_template_entry()
-    entry_2 = insert_dummy_garbage_collection_schedule_template_entry()
+def test_garbage_collection_schedule_template_delete():
+    template = insert_dummy_garbage_collection_schedule_template()
+
+    user = User.objects.create_user(username="test@gmail.com", password="test")
 
     client = APIClient()
-    response = client.get("/garbage_collection_schedule_template_entries/")
+    client.force_login(user)
 
-    response_ids = [e["id"] for e in response.data]
+    response = client.delete(f"/garbage_collection_schedule_templates/{template.id}/")
+    assert response.status_code == 204
 
-    assert sorted(response_ids) == sorted([entry_1.id, entry_2.id])
+    response = client.get(f"/garbage_collection_schedule_templates/{template.id}/")
+    assert response.status_code == 404
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_template_entry_get_detail():
-    entry = insert_dummy_garbage_collection_schedule_template_entry()
+def test_garbage_collection_schedule_template_patch():
+    template = insert_dummy_garbage_collection_schedule_template()
+
+    user = User.objects.create_user(username="test@gmail.com", password="test")
 
     client = APIClient()
-    response = client.get(f"/garbage_collection_schedule_template_entries/{entry.id}/")
+    client.force_login(user)
 
-    assert (
-        response.data["id"] == entry.id
-        and response.data["garbage_type"] == entry.garbage_type.id
-        and response.data["garbage_collection_schedule_template"]
-        == entry.garbage_collection_schedule_template.id
-    )
+    data = {"name": "new name"}
+
+    client.patch(f"/garbage_collection_schedule_templates/{template.id}/", data)
+
+    response = client.get(f"/garbage_collection_schedule_templates/{template.id}/")
+    assert response.data["name"] == "new name"
