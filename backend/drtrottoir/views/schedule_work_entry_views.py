796b13e9305f9da8ee4044b86bd965b422878126
Pim Pieters <pim.pieters@ugent.be>
2023-03-04 22:00:36 +0100

    Removed commented code

--- backend/drtrottoir/views/schedule_work_entry_views.py
+++ backend/drtrottoir/views/schedule_work_entry_views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from drtrottoir.models import ScheduleWorkEntry
 from drtrottoir.serializers import ScheduleWorkEntrySerializer
 
+
 # TODO permissions
 
 
@@ -25,74 +26,3 @@ class ScheduleWorkEntryViewSet(
         work_entries = ScheduleWorkEntry.objects.filter(creator=user_id)
         serializer = self.get_serializer(work_entries, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-"""
-class ScheduleWorkEntryListApiView(APIView):
-    # TODO permission_classes: list = []
-
-    def get(self, request: Request) -> Response:
-        schedule_work_entries = ScheduleWorkEntry.objects
-        serializer = ScheduleWorkEntrySerializer(schedule_work_entries, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request: Request) -> Response:
-        request_user = request.user
-        if request_user is None:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        creation_timestamp = request.data.get("creation_timestamp")
-        image_path = request.data.get("image_path")
-        creator = request.data.get("creator")
-        building = request.data.get("building")
-        schedule_definition = request.data.get("schedule_definition")
-
-        data = {
-            "creation_timestamp": creation_timestamp,
-            "image_path": image_path,
-            "creator": creator,
-            "building": building,
-            "schedule_definition": schedule_definition,
-        }
-
-        serializer = ScheduleWorkEntrySerializer(data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ScheduleWorkEntryApiView(APIView):
-    # TODO permission_classes: list = []
-
-    def get(self, request: Request, schedule_work_entry_id: int) -> Response:
-        try:
-            schedule_work_entry: ScheduleWorkEntry = ScheduleWorkEntry.objects.get(
-                id=schedule_work_entry_id
-            )
-        except ScheduleWorkEntry.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = ScheduleWorkEntrySerializer(schedule_work_entry)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class ScheduleWorkEntryByScheduleDefinitionApiView(APIView):
-    # TODO permission_classes = []
-    def get(self, request: Request, schedule_definition_id: int) -> Response:
-        schedule_work_entries = ScheduleWorkEntry.objects.filter(
-            schedule_definition=schedule_definition_id
-        )
-        serializer = ScheduleWorkEntrySerializer(schedule_work_entries, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class ScheduleWorkEntryByCreatorApiView(APIView):
-    # TODO permission_classes = []
-
-    def get(self, request: Request, creator_id: int) -> Response:
-        schedule_work_entries = ScheduleWorkEntry.objects.filter(creator=creator_id)
-        serializer = ScheduleWorkEntrySerializer(schedule_work_entries, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-"""
