bb76f353893366974aa68b0c742b500654c51b3c
LanderDurie <lander.durie@gmail.com>
2023-03-09 22:55:52 +0100

    method auth, garbage type auth tests

--- backend/drtrottoir/views/garbage_type.py
+++ backend/drtrottoir/views/garbage_type.py
@@ -3,10 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from drtrottoir.models import GarbageType
 from drtrottoir.serializers.garbage_type import GarbageTypeSerializer
+from drtrottoir.permissions import IsSuperstudentOrAdminOrSafe
 
 
 class GarbageTypeViewSet(ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+
+    permission_classes = [permissions.IsAuthenticated, IsSuperstudentOrAdminOrSafe]
 
     queryset = GarbageType.objects.all()
     serializer_class = GarbageTypeSerializer
