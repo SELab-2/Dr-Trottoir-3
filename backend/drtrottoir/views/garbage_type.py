67868d0c3151155e50e3f78b0bbb96ef33f5207f
LanderDurie <lander.durie@gmail.com>
2023-03-11 00:55:48 +0100

    fix flake8 error

--- backend/drtrottoir/views/garbage_type.py
+++ backend/drtrottoir/views/garbage_type.py
@@ -7,6 +7,21 @@ from drtrottoir.serializers.garbage_type import GarbageTypeSerializer
 
 
 class GarbageTypeViewSet(ModelViewSet):
+    """
+    /garbage_types/
+	* GET: (permission student)
+        All garbage types
+	* POST: (permission super_student)
+	    Create a new garbage type, return newly created object.
+    /garbage_types/:garbage_type_id/
+	* GET: (permission student)
+	    Get the garbage type object with the given ID.
+	* PATCH: (permission super_student)
+	    Update the name of the garbage type.
+	* DELETE: (permission super_student)
+	    Delete the garbage type. Only possible if no objects are linked to this garbage type.
+    """
+
     permission_classes = [permissions.IsAuthenticated, IsSuperstudentOrAdminOrSafe]
 
     queryset = GarbageType.objects.all()
