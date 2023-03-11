bfc99b177abaa87a3f7358ae6c59de64d72c6e12
Chewing_Bever <roosensjef@gmail.com>
2023-03-11 16:27:58 +0100

    feat(api): implement more granular permissions system

--- backend/drtrottoir/views/schedule_definition.py
+++ backend/drtrottoir/views/schedule_definition.py
@@ -3,8 +3,16 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from drtrottoir.models import ScheduleDefinition
-from drtrottoir.permissions import IsSuperstudentOrAdmin
-from drtrottoir.serializers import BuildingSerializer, ScheduleDefinitionSerializer
+from drtrottoir.permissions import (
+    HasAssignmentForScheduleDefinition,
+    IsSuperstudentOrAdmin,
+)
+from drtrottoir.serializers import (
+    BuildingSerializer,
+    ScheduleAssignmentSerializer,
+    ScheduleDefinitionSerializer,
+    ScheduleWorkEntrySerializer,
+)
 
 
 class ScheduleDefinitionViewSet(
@@ -13,14 +21,41 @@ class ScheduleDefinitionViewSet(
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
-    permission_classes = [permissions.IsAuthenticated, IsSuperstudentOrAdmin]
+    permission_classes_by_action = {
+        "list": [permissions.IsAuthenticated, IsSuperstudentOrAdmin],
+        "retrieve": [permissions.IsAuthenticated, HasAssignmentForScheduleDefinition],
+        "buildings": [permissions.IsAuthenticated, HasAssignmentForScheduleDefinition],
+        "schedule_work_entries": [permissions.IsAuthenticated, IsSuperstudentOrAdmin],
+        "schedule_assignments": [permissions.IsAuthenticated, IsSuperstudentOrAdmin],
+    }
 
     queryset = ScheduleDefinition.objects.all()
     serializer_class = ScheduleDefinitionSerializer
+
+    def get_permissions(self):
+        return [perm() for perm in self.permission_classes_by_action[self.action]]
 
     @action(detail=True)
     def buildings(self, request, pk=None):
         schedule_definition = self.get_object()
         serializer = BuildingSerializer(schedule_definition.buildings, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def schedule_assignments(self, request, pk=None):
+        schedule_definition = self.get_object()
+        serializer = ScheduleAssignmentSerializer(
+            schedule_definition.schedule_assignments, many=True
+        )
+
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def schedule_work_entries(self, request, pk=None):
+        schedule_definition = self.get_object()
+        serializer = ScheduleWorkEntrySerializer(
+            schedule_definition.work_entries, many=True
+        )
 
         return Response(serializer.data)
