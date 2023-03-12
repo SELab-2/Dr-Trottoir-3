e066177069877067fc6cb1d6bcd958cefa5b363b
bavverst <bavo.verstraeten@ugent.be>
2023-03-12 21:50:29 +0100

    better garbage collection schedule unit tests

--- backend/drtrottoir/tests/test_garbage_collection_schedule_views.py
+++ backend/drtrottoir/tests/test_garbage_collection_schedule_views.py
@@ -1,25 +1,20 @@
 import json
 
 import pytest
+from rest_framework import status
 from rest_framework.test import APIClient
+
+from drtrottoir.models import User
 
 from .dummy_data import (
     insert_dummy_building,
     insert_dummy_garbage_collection_schedule,
-    insert_dummy_garbage_type,
-    insert_dummy_student,
-    insert_dummy_syndicus,
-    insert_dummy_admin,
-    insert_dummy_user
+    insert_dummy_garbage_type, insert_dummy_student, insert_dummy_admin, insert_dummy_user, insert_dummy_syndicus,
 )
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_post_with_correct_rights_returns_201():
-    _test_garbage_collection_schedule_post(insert_dummy_student(is_super_student=True).user)
-
-
-def _test_garbage_collection_schedule_post(user, expected=201, expected_id=1):
+def test_garbage_collection_schedule_post():
     building = insert_dummy_building()
     garbage_type = insert_dummy_garbage_type()
     data = {
@@ -28,122 +23,329 @@ def _test_garbage_collection_schedule_post(user, expected=201, expected_id=1):
         "building": building.id,
     }
 
+    user = insert_dummy_student(is_super_student=True)
+
     client = APIClient()
-    if user is not None:
-        client.force_login(user)
+    client.force_login(user.user)
     response = client.post(
         "/garbage_collection_schedules/",
         json.dumps(data),
         content_type="application/json",
     )
-    if expected < 300:
-        assert response.data == {
-            "id": expected_id,
-            "for_day": "2002-03-17",
-            "garbage_type": garbage_type.id,
-            "building": building.id,
-        }
-    assert response.status_code == expected
+
+    assert response.data == {
+        "id": 1,
+        "for_day": "2002-03-17",
+        "garbage_type": garbage_type.id,
+        "building": building.id,
+    }
+    assert response.status_code == 201
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_get_detail_with_correct_rights_returns_200():
-    _test_garbage_collection_schedule_get_detail(insert_dummy_student(is_super_student=False).user)
-
-
-def _test_garbage_collection_schedule_get_detail(user, expected=200):
+def test_garbage_collection_schedule_get_detail():
     entry = insert_dummy_garbage_collection_schedule()
 
+    user = insert_dummy_student()
+
     client = APIClient()
-    if user is not None:
-        client.force_login(user)
+    client.force_login(user.user)
     response = client.get(f"/garbage_collection_schedules/{entry.id}/")
-    if expected < 300:
-        assert (
-                response.data["id"] == entry.id
-                and response.data["garbage_type"] == entry.garbage_type.id
-                and response.data["building"] == entry.building.id
-        )
-    assert response.status_code == expected
+
+    assert (
+        response.data["id"] == entry.id
+        and response.data["garbage_type"] == entry.garbage_type.id
+        and response.data["building"] == entry.building.id
+        and response.data["for_day"] == str(entry.for_day)
+    )
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_patch_with_correct_rights_returns_200():
-    _test_garbage_collection_schedule_patch(insert_dummy_student(is_super_student=True).user)
-
-
-def _test_garbage_collection_schedule_patch(user, expected=200):
+def test_garbage_collection_schedule_patch():
     entry = insert_dummy_garbage_collection_schedule()
     old_day = entry.for_day
     new_day = "2002-01-09" if old_day != "2002-01-09" else "2002-03-17"
 
+    user = insert_dummy_student(is_super_student=True)
     client = APIClient()
-    if user is not None:
-        client.force_login(user)
+    client.force_login(user.user)
 
     data = {"for_day": new_day}
 
-    patch_response = client.patch(f"/garbage_collection_schedules/{entry.id}/", data)
+    client.patch(f"/garbage_collection_schedules/{entry.id}/", data)
 
-    if expected < 300:
-        response = client.get(f"/garbage_collection_schedules/{entry.id}/")
-        assert response.data["for_day"] == new_day
-    assert patch_response.status_code == expected
+    response = client.get(f"/garbage_collection_schedules/{entry.id}/")
+    assert response.data["for_day"] == new_day
 
 
 @pytest.mark.django_db
-def test_garbage_collection_schedule_delete_with_correct_rights_returns_204():
-    _test_garbage_collection_schedule_delete(insert_dummy_student(is_super_student=True).user)
-
-
-def _test_garbage_collection_schedule_delete(user, expected=204):
+def test_garbage_collection_schedule_delete():
     entry = insert_dummy_garbage_collection_schedule()
+
+    user = insert_dummy_student(is_super_student=True)
+
     client = APIClient()
-    if user is not None:
-        client.force_login(user)
+    client.force_login(user.user)
 
     response = client.delete(f"/garbage_collection_schedules/{entry.id}/")
-    assert response.status_code == expected
-    if expected < 300:
-        response = client.get(f"/garbage_collection_schedules/{entry.id}/")
-        assert response.status_code == 404
+    assert response.status_code == 204
 
+    response = client.get(f"/garbage_collection_schedules/{entry.id}/")
+    assert response.status_code == 404
 
 @pytest.mark.django_db
 def test_garbage_collection_schedule_forbidden_methods():
+    user = insert_dummy_admin()
     client = APIClient()
-    student = insert_dummy_student(is_super_student=True)
-
-    client.force_login(student.user)
+    client.force_login(user.user)
     assert client.get("/garbage_collection_schedules/").status_code == 405
     assert client.patch("/garbage_collection_schedules/").status_code == 405
     assert client.delete("/garbage_collection_schedules/").status_code == 405
     assert client.put("/garbage_collection_schedules/").status_code == 405
 
 
-@pytest.mark.django_db
-def test_garbage_collection_schedule_all_permissions_on_all_allowed_methods():
-    currentid = 0
+def _test_garbage_collection_schedules_list_api_view_post(user=None):
+    """ """
+    client = APIClient()
+    if user is not None:
+        client.force_authenticate(user)
 
-    def _test_post_with_two_arguments(user, expected):
-        nonlocal currentid
-        if expected < 300:
-            currentid += 1
-        _test_garbage_collection_schedule_post(user, expected, currentid)
+    dummy_building = insert_dummy_building()
+    dummy_date = "2002-03-17"
+    dummy_garbage_type = insert_dummy_garbage_type()
 
-    tests = (
-        (_test_post_with_two_arguments, (403, 403, 201, 201, 403,),),
-        (_test_garbage_collection_schedule_get_detail, (403, 200, 200, 200, 403)),
-        (_test_garbage_collection_schedule_patch, (403, 403, 200, 200, 403)),
-        (_test_garbage_collection_schedule_delete, (403, 403, 204, 204, 403)),
+    dummy_data = {"building": dummy_building.id, "for_day": dummy_date, "garbage_type": dummy_garbage_type.id}
+
+    return client.post(
+        "/garbage_collection_schedules/", json.dumps(dummy_data), content_type="application/json"
     )
 
-    student = insert_dummy_student().user
-    superstudent = insert_dummy_student(email="a@a.a", is_super_student=True).user
-    admin = insert_dummy_admin(email="a@a.b", ).user
-    syndicus = insert_dummy_user(email="z@a.a")
-    insert_dummy_syndicus(syndicus)
-    users = (None, student, superstudent, admin, syndicus)
-    for test in tests:
-        for index in range(0, len(users)):
-            test[0](user=users[index], expected=test[1][index])
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_list_api_view_post_no_user_fail():
+    """ """
+    response = _test_garbage_collection_schedules_list_api_view_post()
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_list_api_view_post_student_fail():
+    """ """
+    user = insert_dummy_student()
+
+    response = _test_garbage_collection_schedules_list_api_view_post(user.user)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_list_api_view_post_super_student_success():
+    """ """
+    user = insert_dummy_student(is_super_student=True)
+
+    response = _test_garbage_collection_schedules_list_api_view_post(user.user)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_list_api_view_post_admin_success():
+    """ """
+    user = insert_dummy_admin()
+
+    response = _test_garbage_collection_schedules_list_api_view_post(user.user)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_list_api_view_post_syndicus_fail():
+    """ """
+    dummy_user = insert_dummy_user()
+    user = insert_dummy_syndicus(dummy_user)
+
+    response = _test_garbage_collection_schedules_list_api_view_post(user.user)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    
+def _test_garbage_collection_schedules_detail_api_view_get(user=None):
+    """ """
+    client = APIClient()
+    if user is not None:
+        client.force_authenticate(user)
+
+    dummy = insert_dummy_garbage_collection_schedule()
+
+    return client.get(f"/garbage_collection_schedules/{dummy.id}/")
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_get_no_user_fail():
+    """ """
+    response = _test_garbage_collection_schedules_detail_api_view_get()
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_get_student_success():
+    """ """
+    user = insert_dummy_student()
+
+    response = _test_garbage_collection_schedules_detail_api_view_get(user.user)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_get_syndicus_fail():
+    """ """
+    dummy_user = insert_dummy_user()
+    user = insert_dummy_syndicus(user=dummy_user)
+    response = _test_garbage_collection_schedules_detail_api_view_get(
+        user=user.user
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_get_super_student_success():
+    """ """
+    user = insert_dummy_student(is_super_student=True)
+    response = _test_garbage_collection_schedules_detail_api_view_get(
+        user=user.user
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_get_admin_success():
+    """ """
+    user = insert_dummy_admin()
+    response = _test_garbage_collection_schedules_detail_api_view_get(
+        user=user.user
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    
+    
+def _test_garbage_collection_schedules_detail_api_view_patch(user=None):
+    """ """
+    client = APIClient()
+    if user is not None:
+        client.force_authenticate(user)
+
+    dummy = insert_dummy_garbage_collection_schedule()
+
+    dummy_data = {"for_day": "2002-01-09"}
+
+    return client.patch(
+        f"/garbage_collection_schedules/{dummy.id}/",
+        json.dumps(dummy_data),
+        content_type="application/json",
+    )
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_patch_no_user_fail():
+    """ """
+    response = _test_garbage_collection_schedules_detail_api_view_patch()
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_patch_student_fail():
+    """ """
+    user = insert_dummy_student()
+
+    response = _test_garbage_collection_schedules_detail_api_view_patch(user=user.user)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_patch_super_student_success():
+    """ """
+    user = insert_dummy_student(is_super_student=True)
+
+    response = _test_garbage_collection_schedules_detail_api_view_patch(user.user)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_patch_admin_success():
+    """ """
+    user = insert_dummy_admin()
+
+    response = _test_garbage_collection_schedules_detail_api_view_patch(user.user)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+def _test_garbage_collection_schedules_detail_api_view_delete(user=None):
+    """ """
+    client = APIClient()
+    if user is not None:
+        client.force_authenticate(user)
+
+    dummy = insert_dummy_garbage_collection_schedule()
+
+    return client.delete(f"/garbage_collection_schedules/{dummy.id}/")
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_delete_no_user_fail():
+    """ """
+    response = _test_garbage_collection_schedules_detail_api_view_delete()
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_delete_student_fail():
+    """ """
+    user = insert_dummy_student()
+
+    response = _test_garbage_collection_schedules_detail_api_view_delete(user.user)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_delete_super_student_success():
+    """ """
+    user = insert_dummy_student(is_super_student=True)
+
+    response = _test_garbage_collection_schedules_detail_api_view_delete(user.user)
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_delete_admin_success():
+    """ """
+    user = insert_dummy_admin()
+
+    response = _test_garbage_collection_schedules_detail_api_view_delete(user.user)
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_garbage_collection_schedules_detail_api_view_delete_syndicus_fail():
+    """ """
+    dummy_user = insert_dummy_user()
+    user = insert_dummy_syndicus(user=dummy_user)
+
+    response = _test_garbage_collection_schedules_detail_api_view_delete(
+        user=user.user
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
