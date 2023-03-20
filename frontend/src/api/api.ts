9ea196fba1012b5f34d8eed3cd2189300d395fdc
Chewing_Bever <roosensjef@gmail.com>
2023-03-20 12:23:06 +0100

    feat(frontend): initial draft api abstraction

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,43 @@
+import useSWR from 'swr'
+
+export enum Api {
+    GarbageCollectionScheduleTemplateDetail = "/garbage_collection_schedule_templates/:id/",
+    GarbageCollectionScheduleTemplateDetailEntries = "/garbage_collection_schedule_templates/:id/entries/",
+    GarbageCollectionScheduleTemplateEntryDetail = "/garbage_collection_schedule_template_entries/:id/",
+    GarbageTypes = "/garbage_types/",
+    GarbageTypeDetail = "/garbage_types/:id/",
+    GarbageCollectionScheduleDetail = "/garbage_collection_schedules/:id",
+    LocationGroups = "/location_groups/",
+    LocationGroupDetail = "/location_groups/:id/",
+    LocationGroupDetailBuildings = "/location_groups/:id/buildings/",
+    LocationGroupDetailScheduleDefinitions = "/location_groups/:id/schedule_definitions/",
+    Buildings = "/buildings/",
+    BuildingDetail = "/buildings/:id/",
+    BuildingDetailGarbageCollectionSchedules = "/buildings/:id/garbage_collection_schedules/",
+    BuildingDetailGarbageCollectionScheduleTemplates = "/buildings/:id/garbage_collection_schedule_templates/",
+    BuildingDetailIssues = "/buildings/:id/issues/",
+    BuildingDetailScheduleDefinitions = "/buildings/:id/schedule_definitions/",
+    ScheduleAssignments = "/schedule_assignments/",
+    ScheduleAssignmentDetail = "/schedule_assignments/:id/",
+    ScheduleWorkEntries = "/schedule_work_entries/",
+    ScheduleWorkEntryDetail = "/schedule_work_entries/:id/"
+    ScheduleDefinitions = "/schedule_definitions/",
+    ScheduleDefinitionDetail = "/schedule_definitions/:id/",
+    ScheduleDefinitionDetailBuildings = "/schedule_definitions/:id/buildings/",
+    ScheduleDefinitionDetailScheduleAssignments = "/schedule_definitions/:id/schedule_assignments/",
+    ScheduleDefinitionDetailScheduleWorkEntries = "/schedule_definitions/:id/schedule_work_entries/",
+}
+
+const fetcher<T> = (...args) => fetch(...args).then(res => res.json<T>())
+
+export function get<T>(route: ApiRoute): T {
+    return useSWR<T>(route, fetcher)
+}
+
+export function get_params<T>(route: Api, params: any): T {
+    for (const property in params) {
+        route = route.replace(':' + property, params[property])
+    }
+
+    return useSWR<T>(route, fetcher)
+}
