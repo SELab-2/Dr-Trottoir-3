42899dfcfa7c42175d91cdfa77d27314a5b4f32f
LanderDurie <lander.durie@gmail.com>
2023-03-20 13:06:20 +0100

    remove linting from unused default next.js template

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import {Inter} from '@next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+// eslint-disable-next-line new-cap
+const inter = Inter({subsets: ['latin']});
 
+// eslint-disable-next-line require-jsdoc
 export default function Home() {
   return (
     <>
@@ -119,5 +121,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
