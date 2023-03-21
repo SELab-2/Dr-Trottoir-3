3dd607df2837059f82379e2a6d60ea06f86b01cb
LanderDurie <lander.durie@gmail.com>
2023-03-21 13:01:15 +0100

    initial navbar

---
+++ frontend/src/components/elements/navbarElement/navbar.tsx
@@ -0,0 +1,79 @@
+import styles from './navbar.module.css';
+import Button from '@mui/material/Button';
+import React from 'react';
+
+// eslint-disable-next-line require-jsdoc
+// @ts-ignore
+// eslint-disable-next-line require-jsdoc
+export default function NavBar(props) {
+  const [selectedBtn, setSelectedBtn] = React.useState(0);
+
+  return (
+    <div className={styles.full}>
+      <div className={styles.row_flex_container}>
+        {/* eslint-disable-next-line max-len */}
+        <div className={styles.left_flex_container} style={{backgroundColor: 'green'}}>
+          <div className={styles.side_bar_top}></div>
+          <div className={styles.side_bar_mid}>
+            <Button className={styles.button}
+              color={selectedBtn === 0 ? 'secondary' : 'primary'}
+              onClick={()=>setSelectedBtn(0)}
+            >
+              Planner
+            </Button>
+            <Button className={styles.button}
+              color={selectedBtn === 1 ? 'secondary' : 'primary'}
+              onClick={()=>setSelectedBtn(1)}
+            >
+              Actieve Routes
+            </Button>
+            <Button className={styles.button}
+              color={selectedBtn === 2 ? 'secondary' : 'primary'}
+              onClick={()=>setSelectedBtn(2)}
+            >
+              Gebruikers
+            </Button>
+            <Button className={styles.button}
+              color={selectedBtn === 3 ? 'secondary' : 'primary'}
+              onClick={()=>setSelectedBtn(3)}
+            >
+              Routes
+            </Button>
+            <Button className={styles.button}
+              color={selectedBtn === 4 ? 'secondary' : 'primary'}
+              onClick={()=>setSelectedBtn(4)}
+            >
+              Gebouwen
+            </Button>
+          </div>
+          <div className={styles.side_bar_bot}>
+            <Button className={styles.button}
+                    color={selectedBtn === 5 ? 'secondary' : 'primary'}
+                    onClick={()=>setSelectedBtn(5)}
+            >
+              Account
+            </Button>
+            <Button className={styles.button}
+                    color={selectedBtn === 6 ? 'secondary' : 'primary'}
+                    onClick={()=>setSelectedBtn(6)}
+            >
+              Instellingen
+            </Button>
+            <Button className={styles.button}
+                    color={selectedBtn === 7 ? 'secondary' : 'primary'}
+                    onClick={()=>setSelectedBtn(7)}
+            >
+              Logout
+            </Button>
+          </div>
+        </div>
+        <div className={styles.right_flex_container} style={{backgroundColor: 'red'}}>
+          <div className={styles.top_bar}></div>
+          <div className={styles.content_space}>
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
