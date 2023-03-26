b5a2466f1f5d7ed1b703e257c3d36002007182db
joripeet <joris.peeters@ugent.be>
2023-03-26 01:02:26 +0100

    static files dir as env variable

--- backend/drtrottoir/settings.py
+++ backend/drtrottoir/settings.py
@@ -138,8 +138,9 @@ BASE_PATH = os.environ.get("BASE_PATH", "")
 STATIC_URL = BASE_PATH + "static/static/"
 MEDIA_URL = BASE_PATH + "static/media/"
 
-STATIC_ROOT = "/vol/web/static"
-MEDIA_ROOT = "/vol/web/media"
+STATIC_ROOT_BASE = os.environ.get("STATIC_DATA_DIR", "./static_files/")
+STATIC_ROOT = STATIC_ROOT_BASE + "static"
+MEDIA_ROOT = STATIC_ROOT_BASE + "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
