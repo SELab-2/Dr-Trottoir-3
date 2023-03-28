caa89c4a6489bc95f26330676b14914d10b5b777
LanderDurie <lander.durie@gmail.com>
2023-03-28 11:48:02 +0200

    add reactive webapp template for navbar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,12 +3,12 @@ import '@/styles/main_stylesheet.css';
 import type {AppProps} from 'next/app';
 import {SessionProvider} from 'next-auth/react';
 
-import NavBar from '../components/elements/navbarElement/navbar';
+import Navbar from '../components/elements/navbarElement/Navbar';
 
 export default function App(
     {Component, pageProps: {session, ...pageProps}}: AppProps
 ) {
-    const getLayout = (props: any) => <NavBar>{props}</NavBar>;
+    const getLayout = (props: any) => <Navbar>{props}</Navbar>;
 
     return (
         <SessionProvider session={session} basePath={'http://localhost:3002/api/auth'}>
