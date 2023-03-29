132106b0237b93b39227ddac21e5b13603b99457
Jef Roosens <roosensjef@gmail.com>
2023-03-29 19:25:17 +0200

    feat(api): add auto swagger docs

--- backend/drtrottoir/settings.py
+++ backend/drtrottoir/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    "drf_yasg",
     "drtrottoir",
 ]
 
--- backend/drtrottoir/urls.py
+++ backend/drtrottoir/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -36,6 +39,16 @@ from drtrottoir.views import (
     ScheduleDefinitionViewSet,
     ScheduleWorkEntryViewSet,
     UserViewSet,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dr. Trottoir Group 3",
+        default_version="v1",
+        description="",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 router = DefaultRouter()
@@ -105,6 +118,19 @@ urlpatterns = [
         settings.BASE_PATH + "auth/token/refresh/",
         TokenRefreshView.as_view(),
         name="token_refresh",
+    ),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
 ]
 
