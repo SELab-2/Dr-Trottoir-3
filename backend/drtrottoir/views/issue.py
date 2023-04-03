573f640bc99baa4f58befc6beaba4e93c608700a
Chewing_Bever <roosensjef@gmail.com>
2023-04-03 14:31:04 +0200

    fix: pass issue tests

--- backend/drtrottoir/views/issue.py
+++ backend/drtrottoir/views/issue.py
@@ -24,20 +24,17 @@ from .mixins import PermissionsByActionMixin
 class IssueViewSet(PermissionsByActionMixin, viewsets.ModelViewSet):
     serializer_class = IssueSerializer
 
-    permission_classes = [permissions.IsAuthenticated, IsSuperstudentOrAdmin]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsStudent | IsSuperstudentOrAdmin | IsSyndicus,
+    ]
     permission_classes_by_action = {
-        "list": [
-            permissions.IsAuthenticated,
-            IsStudent | IsSuperstudentOrAdmin | IsSyndicus,
-        ],
-        "not_approved": [
-            permissions.IsAuthenticated,
-            IsStudent | IsSuperstudentOrAdmin | IsSyndicus,
-        ],
         "create": [permissions.IsAuthenticated, IsStudent | IsSuperstudentOrAdmin],
-        "update": [
+        "update": [permissions.IsAuthenticated, IsSuperstudentOrAdmin],
+        "partial_update": [permissions.IsAuthenticated, IsSuperstudentOrAdmin],
+        "destroy": [
             permissions.IsAuthenticated,
-            IsStudent | IsSuperstudentOrAdmin | IsSyndicus,
+            IsSuperstudentOrAdmin | IsSyndicusOfBuildingAndApprovalNotNull,
         ],
     }
 
@@ -58,6 +55,18 @@ class IssueViewSet(PermissionsByActionMixin, viewsets.ModelViewSet):
         # Only other option is a regular student
         else:
             return Issue.objects.filter(from_user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        request.data["from_user"] = request.user.id
+
+        return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        issue = self.get_object()
+        issue.resolved = True
+        issue.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=False)
     def not_approved(self, request):
