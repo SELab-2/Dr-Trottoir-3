45493e9eaaa108f6743c097aa80a9973c994f589
bavverst <bavo.verstraeten@ugent.be>
2023-04-13 01:34:18 +0200

    fixing linting on signout

--- frontend/src/components/elements/LogoutPageElement/LogoutPageElement.tsx
+++ frontend/src/components/elements/LogoutPageElement/LogoutPageElement.tsx
@@ -4,35 +4,31 @@ import {Button} from '@mui/material';
 import React from 'react';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
-import { signOut } from "next-auth/react"
-
+import {signOut} from 'next-auth/react';
 
 
 export default function LogoutPageElement() {
     return (
-
-            <div className={styles.full}>
-                uitloggen
-                <div className={styles.button_holder}>
-
-                    <Button id={styles.button}
-                        onClick={() =>
-                            signOut({ callbackUrl: '/login' })
-                        }
-                        className={styles.button_default}
-                    >
-                        <CheckIcon className={styles.icon}/>
-                    </Button>
-                    <Button id={styles.button}
-                        onClick={() => {
-                            Router.push('/', undefined, {shallow: true}).then();
-                        }}
-                        className={styles.button_default}
-                    >
-                        <CloseIcon className={styles.icon}/>
-                    </Button>
-                </div>
+        <div className={styles.full}>
+            uitloggen
+            <div className={styles.button_holder}>
+                <Button id={styles.button}
+                    onClick={() =>
+                        signOut({callbackUrl: '/login'})
+                    }
+                    className={styles.button_default}
+                >
+                    <CheckIcon className={styles.icon}/>
+                </Button>
+                <Button id={styles.button}
+                    onClick={() => {
+                        Router.push('/', undefined, {shallow: true}).then();
+                    }}
+                    className={styles.button_default}
+                >
+                    <CloseIcon className={styles.icon}/>
+                </Button>
             </div>
-
+        </div>
     );
 }
