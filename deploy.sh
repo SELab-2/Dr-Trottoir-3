7e34ea129a38f1f7c42e0e3f8df1e854392cb988
Joris-Peeters <joris.peeters1001@gmail.com>
2023-04-19 12:36:37 +0200

    updated deployment config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Not ready for deployment
-exit 1
 cd caddy || exit
+set -a
+source ../.env.prod
 docker compose -f docker-compose.prod.yml -p drtrottoir build
 docker compose -f docker-compose.prod.yml -p drtrottoir up -d --force-recreate
