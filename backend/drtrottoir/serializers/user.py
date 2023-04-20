a38eadc6ce06f42895820a57328e7d823acc1fed
Chewing_Bever <roosensjef@gmail.com>
2023-04-20 16:33:25 +0200

    feat: allow updating user relationships using PATCH

--- backend/drtrottoir/serializers/user.py
+++ backend/drtrottoir/serializers/user.py
@@ -42,6 +42,7 @@ class UserSerializer(serializers.ModelSerializer):
             "admin",
             "syndicus",
         ]
+        read_only_fields = ["id"]
 
     def create(self, validated_data):
         student_data = validated_data.pop("student")
@@ -60,3 +61,56 @@ class UserSerializer(serializers.ModelSerializer):
             Syndicus.objects.create(user=user, **syndicus_data)
 
         return user
+
+    @staticmethod
+    def update_one_on_one_field(instance, validated_data, field_name, field_model):
+        """
+        Utility function for the `update` method that updates a one-on-one
+        field's reverse relationship. It handles the following cases:
+        * New data is provided for a relationship that doesn't exist yet
+        * New data is provided for an existing relationship
+        * An existing relationship is set to null, meaning it should be removed
+        """
+        if field_name in validated_data:
+            field_data = validated_data.pop(field_name)
+            old_relation_exists = hasattr(instance, field_name)
+            new_relation_is_null = field_data is None
+
+            # Relation exists, but the new data is None -> remove relation
+            if old_relation_exists and new_relation_is_null:
+                getattr(instance, field_name).delete()
+                setattr(instance, field_name, None)
+
+            # Relation already exists, and data is provided -> update fields
+            elif old_relation_exists and not new_relation_is_null:
+                relation_instance = getattr(instance, field_name)
+
+                for field, value in field_data.items():
+                    setattr(relation_instance, field, value)
+
+                relation_instance.save(update_fields=field_data.keys())
+
+            # Relationship doesn't exist, but data is provided -> create
+            # relation
+            elif not new_relation_is_null:
+                field_model.objects.create(user=instance, **field_data)
+
+            # Final option is where both are null, meaning nothing has to be
+            # done
+
+    def update(self, instance, validated_data):
+        UserSerializer.update_one_on_one_field(
+            instance, validated_data, "student", Student
+        )
+        UserSerializer.update_one_on_one_field(instance, validated_data, "admin", Admin)
+        UserSerializer.update_one_on_one_field(
+            instance, validated_data, "syndicus", Syndicus
+        )
+
+        # Remaining data is what should be updated on the User instance itself
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+
+        instance.save(update_fields=validated_data.keys())
+
+        return instance
