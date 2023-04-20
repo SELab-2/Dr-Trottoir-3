86d58a89dccd977b91d35afd38ec0d6867b84b0f
Chewing_Bever <roosensjef@gmail.com>
2023-04-20 15:09:53 +0200

    fix: allow Users to be created

--- backend/drtrottoir/serializers/user.py
+++ backend/drtrottoir/serializers/user.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from rest_framework import serializers
 
 from drtrottoir.models import Admin, Student, Syndicus, User
@@ -6,26 +8,55 @@ from drtrottoir.models import Admin, Student, Syndicus, User
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
-        fields = "__all__"
+        fields: List[str] = []
+        read_only_fields = ["id", "user"]
 
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = "__all__"
+        fields = ["location_group", "is_super_student"]
+        read_only_fields = ["id", "user"]
 
 
 class SyndicusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Syndicus
-        fields = "__all__"
+        fields = ["buildings"]
+        read_only_fields = ["id", "user"]
 
 
 class UserSerializer(serializers.ModelSerializer):
-    student = StudentSerializer()
-    admin = AdminSerializer()
-    syndicus = SyndicusSerializer()
+    student = StudentSerializer(allow_null=True)
+    admin = AdminSerializer(allow_null=True)
+    syndicus = SyndicusSerializer(allow_null=True)
 
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "student", "admin", "syndicus"]
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "student",
+            "admin",
+            "syndicus",
+        ]
+
+    def create(self, validated_data):
+        student_data = validated_data.pop("student")
+        admin_data = validated_data.pop("admin")
+        syndicus_data = validated_data.pop("syndicus")
+
+        user = User.objects.create(**validated_data)
+
+        if student_data is not None:
+            Student.objects.create(user=user, **student_data)
+
+        if admin_data is not None:
+            Admin.objects.create(user=user, **admin_data)
+
+        if syndicus_data is not None:
+            Syndicus.objects.create(user=user, **syndicus_data)
+
+        return user
