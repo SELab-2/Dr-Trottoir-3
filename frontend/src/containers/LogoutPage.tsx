158af126890fd8fe1182af4bfb4bf47167c2ccca
Maxim Stockmans <contact@maximstockmans.com>
2023-05-06 09:52:00 +0200

    fix: linting

--- frontend/src/containers/LogoutPage.tsx
+++ frontend/src/containers/LogoutPage.tsx
@@ -1,11 +1,10 @@
-import LogoutPageElement from '@/components/elements/LogoutPageElement/LogoutPageElement';
 import Head from 'next/head';
 import React, {useEffect} from 'react';
-import {signOut} from "next-auth/react";
+import {signOut} from 'next-auth/react';
 
 export default function LogoutPage() {
     useEffect(() => {
-        signOut({callbackUrl: '/login'})
+        signOut({callbackUrl: '/login'});
     }, []);
 
     return (
